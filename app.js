4edd20c660d0bbc7fbeb203f1406c3b80080577d
Kushwaha <karvika.kushwaha@dxc.com>
2025-01-20 18:02:38 +0530

    Routing

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const express = require('express')
 
 const app = express()
 //use allow us to add a new middleware function
-app.use((req, res, next)=>{
-    console.log("In the middleware");
-    next();   // allows the request to continue with the next middleware in line
+app.use("/add-product",(req, res, next)=>{
+    console.log("On the product page");
+    res.send('<h1> Hello from Product! </h1>');
 }) 
-app.use((req, res, next)=>{
-    console.log("In the another middleware");
+app.use("/",(req, res, next)=>{
+    console.log("In the middleware");
     res.send('<h1> Hello from Express! </h1>');
 }) 
 const server = http.createServer(app);
