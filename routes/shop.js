4fb49518667a7e0eeccc0584816db14c848afed1
Kushwaha <karvika.kushwaha@dxc.com>
2025-01-21 12:14:10 +0530

    Add styling

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/",(req, res, next)=>{
     console.log("In the middleware");
     // can't use directly path like ./view/shop.html as it takes root folder of OS not the given directory
-    res.sendFile(path.join(__dirname,'../','view','shop.html'));
+    res.sendFile(path.join(__dirname,'..','view','shop.html'));
 }) 
 
 module.exports = router
